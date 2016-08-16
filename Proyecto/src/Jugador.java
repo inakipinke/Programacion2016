3ae0708d242599f39c29a432b4b0beada5bb9122
MirandaGonzalo <gonzamirandab2000@gmail.com>
2016-08-16 13:30:32 -0300

    Clase del Jugador

--- Proyecto/src/Jugador.java
+++ Proyecto/src/Jugador.java
@@ -1,23 +1,24 @@
 
 public class Jugador {
 
-    public String nombre;
-    public String apellido;
-    public int año_de_nacimiento;
-    public int mes_de_nacimiento;
-    public int día_de_nacimiento;
-    public String nacionalidad;
-    public Float altura;
-    public Float peso;
+    private String nombre;
+    private String apellido;
+    private int año_de_nacimiento;
+    private int mes_de_nacimiento;
+    private int día_de_nacimiento;
+    private String nacionalidad;
+    private Float altura;
+    private Float peso;
     private Puntos puntos;
     private Puntos games;
+    private Puntos sets;
 
     //EL JUGADOR TIENE SU PROPIO MARCADOR DONDE SE VEN REFLEJADOS LOS PUNTOS QUE HAYA ANOTADO.
     
     public Jugador() {
     }
 
-    public Jugador(String nombre, Puntos puntos,Puntos games, String apellido, int año_de_nacimiento, int mes_de_nacimiento, int día_de_nacimiento, String nacionalidad, Float altura, Float peso) {
+    public Jugador(String nombre, Puntos puntos,Puntos games,Puntos sets, String apellido, int año_de_nacimiento, int mes_de_nacimiento, int día_de_nacimiento, String nacionalidad, Float altura, Float peso) {
         this.nombre = nombre;
         this.año_de_nacimiento = año_de_nacimiento;
         this.mes_de_nacimiento = mes_de_nacimiento;
@@ -28,6 +29,15 @@ public class Jugador {
         this.peso = peso;
         this.puntos = new Puntos();
         this.games = new Puntos();
+        this.sets = new Puntos();
+    }
+
+    public Puntos getSets() {
+        return sets;
+    }
+
+    public void setSets(Puntos sets) {
+        this.sets = sets;
     }
 
     public String getNombre() {
@@ -109,34 +119,42 @@ public class Jugador {
     public void setGames(Puntos games) {
         this.games = games;
     }
-    
-    
+ 
     public void aumentar_Puntos() {
-        int puntos_aux = 0 ;
-        if (puntos.getPuntos() == puntos_aux) {
+        if (puntos.getPuntos() == 0) {
             puntos.setPuntos(15);
+            System.err.println(nombre + puntos.getPuntos() + "PUNTOS");
+            return;
 
         }
         if (puntos.getPuntos() == 15) {
-            puntos.setPuntos(puntos.getPuntos() + 15);
+            puntos.setPuntos(30);
+            System.err.println(nombre + puntos.getPuntos() + "PUNTOS");
+            return;
 
         }
         if (puntos.getPuntos() == 30) {
-            puntos.setPuntos(puntos.getPuntos() + 10);
+            puntos.setPuntos(40);
+            System.err.println(nombre + puntos.getPuntos() + "PUNTOS");
+            return;
 
         }
         if (puntos.getPuntos() == 40) {
-            aumentar_games();
+            Partido.game_ganado();
+            return;
         }
     }
 
     public void aumentar_games() {        
-        puntos.setGames(puntos.getGames() + 1); 
+        puntos.setGames(puntos.getGames() + 1);
+        System.err.println(nombre + "" + puntos.getGames() + " GAMES ");
         puntos.setPuntos(puntos.getPuntos() - puntos.getPuntos());
+        System.out.println(puntos.getPuntos() + "");
         if (puntos.getGames() == 6) {
             aumentar_sets();
+            return;
         }
-        
+        return;
     }
 
     public void aumentar_sets() {
@@ -162,4 +180,5 @@ public class Jugador {
         System.out.println(puntos.getPuntos() + "");
     }
 
+   
 }
